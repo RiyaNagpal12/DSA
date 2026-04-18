9f8fcdffb794e2f88793d2c00fc6a9a02929feb7
RiyaNagpal12 <riyanagpal9c@gmail.com>
2026-04-18 21:16:56 +0530

    Refactor binary tree node class and sum method
    Refactored node class and updated sum method to use Node.

--- BinaryTree/Sum.java
+++ BinaryTree/Sum.java
@@ -1,17 +1,32 @@
-public class Sum {
-  public static class  node {
-    int data ;
-    node left ;
-    node right ;
+/*✔ Problem: Find sum of all nodes in a binary tree
 
-    public node (int data ){
+✔ Approach:
+- Used recursion (post-order traversal)
+- Calculated sum of left subtree
+- Calculated sum of right subtree
+- Added root node value
+
+✔ Complexity:
+- Time: O(n)
+- Space: O(h) (height of tree)
+
+✔ Key Concept:
+Tree traversal using recursion*/
+public class Sum {
+  public static class  Node {
+    int data ;
+    Node left ;
+    Node right ;
+
+    public Node (int data ){
       this.data = data;
       left = null;
       right = null;
   }
+  }
 
 
-    public static int sum (node root  ){
+    public static int sum (Node root  ){
 
       if(root == null){
         return 0 ;}
@@ -24,6 +39,15 @@ public class Sum {
 
 
     public static void main(String[] args){
+        Node root = new Node(1);
+    root.left = new Node(2);
+    root.right = new Node(3);
+    root.left.left = new Node(4);
+    root.left.right = new Node(5);
+
+    System.out.println("Sum of tree : " +sum(root));
+  }
+}
       
       
 
