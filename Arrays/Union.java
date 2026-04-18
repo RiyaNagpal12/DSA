3875100beae053ffe6de22f838e873e96e942c1d
RiyaNagpal12 <riyanagpal9c@gmail.com>
2026-04-18 21:07:44 +0530

    Create Union.java

---
+++ Arrays/Union.java
@@ -0,0 +1,82 @@
+import java.util.*;
+
+public class UnionArray {
+
+    public static int[] union(int arr1[], int arr2[]) {
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        int i = 0, j = 0;
+        ArrayList<Integer> result = new ArrayList<>();
+
+        while (i < arr1.length && j < arr2.length) {
+          if(arr1[i]<arr2[j]){
+      
+
+              if(result.isEmpty()  || result.get(result.size() -1 ) != arr1[i]){
+                result.add(arr1[i]);
+
+              }
+            i++ ; 
+
+              }
+
+          else if (arr2[j]<arr1[i]){
+            if(result.isEmpty() ||  result.get(result.size() -1) != arr2[j]){
+              result.add( arr2[j]) ;
+            }
+            j++;
+          }
+
+          else {
+            if(result.isEmpty() || result.get(result.size() -1 ) != arr1[i] ){
+              result.add(arr1[i]);
+
+            }
+            i++;
+            j++;}
+        }
+
+          while( i<arr1.length){
+            
+              if(result.isEmpty()  || result.get(result.size() -1 ) != arr1[i]){
+                result.add(arr1[i]);
+
+              }
+
+            i++;
+          }
+
+          while (j<arr2.length ){
+              if(result.isEmpty() ||  result.get(result.size() -1) != arr2[j]){
+              result.add( arr2[j]) ;
+            }
+            j++;
+          }
+
+
+
+          
+            
+
+        int[] ans = new int[result.size()];
+        for (int k = 0; k < result.size(); k++) {
+            ans[k] = result.get(k);
+        }
+
+        return ans;
+    }
+}
+
+public static void main(String[] args) {
+    int[] arr1 = {1,2,2,1};
+    int[] arr2 = {2,3,4};
+
+    int[] res = union(arr1, arr2);
+
+    System.out.print("Union: ");
+    for (int num : res) {
+        System.out.print(num + " ");
+    }
+}
