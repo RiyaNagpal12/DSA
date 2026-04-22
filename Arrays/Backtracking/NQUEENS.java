26c0e3f86c4cf9e3b3b58a56dc3c23e48eed8c66
RiyaNagpal12 <riyanagpal9c@gmail.com>
2026-04-22 21:53:34 +0530

    Create NQUEENS.java

---
+++ Arrays/Backtracking/NQUEENS.java
@@ -0,0 +1,84 @@
+// Place NQUEEN IN NXn chessboard such that no 2 queens can attack each other 
+public class ques31 {
+    public static boolean issafe(char board[][] , int row , int col){
+
+        // vertical 
+        for(int i = row -1  ; i>=0 ; i--){
+            if(board[i][col]== 'Q'){
+                return false;
+            }
+
+        }
+        // left diagnol 
+        for(int i = row-1,  j = col-1 ; i>=0 && j>=0 ;i--,j--){
+            if(board[i][j]== 'Q'){
+                return false ;
+            }
+
+
+        }
+        
+        // right 
+        for(int i = row-1 ,j= col+1 ; i>=0 && j<board[0].length ; i-- , j++){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+
+        }
+        return true ;
+         
+    }
+    public static void nqueens(char board[][] , int row){
+        
+        if(row== board.length){
+            printboard(board);
+            count ++;
+            return ;
+        }
+
+        
+        for(int j =0 ;j<board.length; j++){
+
+            if(issafe(board , row , j) == true){
+                board[row][j] = 'Q';
+                nqueens(board, row+1);
+                board[row][j]='x';
+
+                }
+       
+        }
+        
+
+
+    }
+    static int count = 0;
+
+    public static void printboard(char  board[][]){
+        System.out.println("Chess board");
+        for(int i=0;i<board.length;i++){
+            for(int j =0;j<board.length;j++){
+                System.out.print(board[i][j]+" ");
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+    }
+       public static void main(String[] args) {
+           int n =4 ;
+           char board[][] = new char[n][n];
+            // intialise with 0 queens 
+
+            for (int i = 0; i < n; i++) {
+                for(int j =0;j<n ;j++){
+                   board[i][j]='x';
+
+                }
+
+                
+            }
+            
+                nqueens(board ,0);
+                System.out.println("Total ways to place queen is:" +  count);
+       }
+    
+}
