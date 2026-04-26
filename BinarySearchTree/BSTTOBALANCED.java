798b242fa41d00ee6f454d7e26d042d7f6b46990
RiyaNagpal12 <riyanagpal9c@gmail.com>
2026-04-26 22:22:38 +0530

    Add BST to Balanced BST conversion functionality
    Implemented a method to convert a Binary Search Tree (BST) into a balanced BST using inorder traversal and divide & conquer approach.

---
+++ BinarySearchTree/BSTTOBALANCED.java
@@ -0,0 +1,63 @@
+
+/* ✔ Problem: Convert BST to Balanced BST
+
+✔ Approach:
+- Performed inorder traversal to get sorted elements
+- Used divide & conquer to build balanced BST
+- Selected middle element as root for height balance
+
+✔ Complexity:
+- Time: O(n)
+- Space: O(n)
+
+✔ Key Concept:
+BST → Sorted Array → Balanced BST */
+
+import java.util.ArrayList;
+
+// convertedbst to balanced bst 
+public class q72 {
+
+    public static class node {
+        int data;
+        node left;
+        node right ;
+
+        public node(int data){
+            this.data = data ;
+            left = null;
+            right =  null;
+
+
+        }
+    }
+
+        public static void   inorder(node root , ArrayList<Integer> list ){
+
+            if(root == null){
+                return;
+            }
+            
+            inorder(root.left , list);
+            list.add(root.data);
+            inorder(root.right, list);
+
+        }
+
+        public static node  createbst( ArrayList<Integer> list , int start , int end  ){
+            if (start > end) return null;
+
+            int mid =(start+end)/2;
+            node root = new node(list.get(mid));
+            root.left =createbst(list , start , mid-1);
+            root.right = createbst(list ,   mid+1 , end);
+            return root ;
+
+        }
+
+        public static node convertbst(node root ){
+            ArrayList<Integer> list = new ArrayList<>();
+            inorder(root , list);
+            return  createbst(list, 0, list.size()-1);
+
+        }
