424535492845921f97cc65c6eeeaf9640d216971
Riya Nagpal <riyanagpal9c@gmail.com>
2026-05-01 22:27:31 +0530

    Create MaxSum.java

---
+++ Queue/MaxSum.java
@@ -0,0 +1,38 @@
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class q30 {
+
+    public static int maxsum(int arr[] ,int k){
+        int sum=0;
+        int maxsum =Integer.MIN_VALUE;
+        Queue<Integer> q = new LinkedList<>();
+        for(int i =0;i<arr.length;i++){
+            q.add(arr[i]);
+            sum+=arr[i];
+
+            if(q.size()>k){
+                int val =q.remove();
+                sum-=val;
+            }
+
+            if(q.size()==k){
+                maxsum=Math.max(sum , maxsum);
+            }
+
+
+          
+    }
+
+return maxsum;}
+public static void main(String[] args) {
+     int arr[] = {2, 1, 5, 1, 3, 2};
+        int k = 3;
+        
+        System.out.println(maxsum(arr, k)); // Output: 9
+    }
+        
+    }
+    
+    
