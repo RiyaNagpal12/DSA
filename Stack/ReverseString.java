0ce7db697cddf93b6fdb9cc4ba284e80106bc437
Riya Nagpal <riyanagpal9c@gmail.com>
2026-05-02 22:44:50 +0530

    Create ReverseString.java

---
+++ Stack/ReverseString.java
@@ -0,0 +1,76 @@
+
+import java.util.ArrayList;
+
+// reverse a string using stack 
+
+public class q11 {
+
+    static class stack{
+        static ArrayList<Character> list = new ArrayList<>();
+        public boolean isempty()
+{    if (list.size()==0){
+    return true;
+
+
+}
+return false;
+
+}        public void push(char data){
+            list.add(data);
+
+        }
+
+        public char pop(){
+            if(list.isempty() == true){
+                return '\0';
+            }
+            char top = list.get(list.size()-1);
+            list.remove(list.size()-1);
+            return top;
+        }
+
+
+        public char peek(){
+            if(list.isEmpty()){
+                return '\0';
+            }
+
+            char top = list.get(list.size()-1);
+        return top;        }
+
+
+    }
+
+
+    public static void reversestring(String str   ){
+
+        stack st = new stack();
+        int i=0;
+        
+        while(i< str.length()){
+
+            st.push(str.charAt(i));
+            i++;
+
+
+            
+
+
+        }
+        StringBuilder st1 = new StringBuilder();
+        while(st.isempty()== false){
+            
+        st1.append(st.pop());
+
+        }
+        System.out.println(st1.toString());
+
+
+    }
+
+    public static void main(String[] args){
+        String str = "abc";
+        reversestring(str);
+
+    }
+}
