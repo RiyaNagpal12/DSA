b0e8177b8fe80edfc9b14f1f639241a99d6102a5
Riya Nagpal <riyanagpal9c@gmail.com>
2026-05-04 23:21:53 +0530

    Update FindMissing.java

--- Arrays/Striver/FindMissing.java
+++ Arrays/Striver/FindMissing.java
@@ -2,13 +2,14 @@ import java.util.*;
 public class FindMissing{ 
   public static int find(int arr[]){
     Arrays.sort(arr);
-    int result ; 
+    int result =0; 
     for (int i=1;i<arr.length ;i++){
       if (arr[i]!=arr[i-1]+1){
         result =arr[i-1]+1;
         break;
       }
     } 
-    return result}
+    return result; 
+  }
         
         
