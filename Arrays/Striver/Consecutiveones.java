e54fd56645ae748c4da49c340f5bceaaf544228e
Riya Nagpal <riyanagpal9c@gmail.com>
2026-05-04 22:49:40 +0530

    Update Consecutiveones.java

--- Arrays/Striver/Consecutiveones.java
+++ Arrays/Striver/Consecutiveones.java
@@ -1,1 +1,19 @@
+import java.util.*;
+public static class ConsecutiveOnes{ 
+  public static int count (int arr[]){ 
+    int count =0; 
+    int max_count =0; 
+    for(int i=0 ;i<arr.length;i++){
+      if (arr[i]==1){
+        count ++;
+        max_count = Math.max(count,max_count);
+      } 
+      elseif {
+        count=0; 
 
+    }
+  }
+  return max_count ; 
+  } 
+  
+      
