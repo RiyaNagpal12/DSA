510786b56b54a0a9e342e2310f1b65fa0fff7400
Riya Nagpal <riyanagpal9c@gmail.com>
2026-05-04 22:36:43 +0530

    Create movezeroes.java

---
+++ Arrays/Striver/movezeroes.java
@@ -0,0 +1,17 @@
+public static class Movezeroes {
+  public static void movezeroes (int arr[]){
+    int j =0;
+    
+    for(int i = 0 ;i, arr.length ;i++){
+      if(arr[i]!=0){
+        int temp =arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+
+
+        j++;
+      }
+    }
+  }
+  
+    
