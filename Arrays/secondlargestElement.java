e202d1596e09b104c1603ca4e8f902223a1825d6
Riya Nagpal <riyanagpal9c@gmail.com>
2026-05-04 22:18:26 +0530

    Create secondlargestElement.java

---
+++ Arrays/secondlargestElement.java
@@ -0,0 +1,21 @@
+public class secondlargest {
+
+    public static int findsecondlargest(int arr[]) {
+        int largest_Element = Integer.MIN_VALUE;
+        int second_largest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] > largest_Element) {
+                second_largest = largest_Element;
+                largest_Element = arr[i];
+            } 
+            else if (arr[i] > second_largest && arr[i] != largest_Element) {
+                second_largest = arr[i];
+            }
+        }
+
+        return second_largest;
+    }
+}
+      
