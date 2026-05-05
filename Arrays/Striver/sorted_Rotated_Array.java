1ce4b362ba1214ef86207e6ada5da725b4708bdf
Riya Nagpal <riyanagpal9c@gmail.com>
2026-05-05 16:08:46 +0530

    Create sorted_Rotated_Array.java

---
+++ Arrays/Striver/sorted_Rotated_Array.java
@@ -0,0 +1,13 @@
+public static boolean isRotatedSorted(int arr[]) {
+    int count = 0;
+    int n = arr.length;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > arr[(i + 1) % n]) {
+            count++;
+        }
+    }
+
+    return count <= 1;
+}
+
