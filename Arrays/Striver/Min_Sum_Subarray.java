b040af3daff273a1296654e530e9c2b84469e48c
Riya Nagpal <riyanagpal9c@gmail.com>
2026-05-06 22:42:07 +0530

    Create Min_Sum_Subarray.java

---
+++ Arrays/Striver/Min_Sum_Subarray.java
@@ -0,0 +1,13 @@
+public class MinSumSubarray{
+  public static int minsum(int arr[]){
+    int currentsum =0;
+    int minsum=Integer.MAX_VALUE;
+    for(int i=0; i<arr.length ;i++){
+      currentsum += arr[i];
+      minsum = Math.min(currentsum , minsum);
+
+    if(currentsum >0){
+      currentsum =0;}
+    }
+    return minsum ;}
+  
