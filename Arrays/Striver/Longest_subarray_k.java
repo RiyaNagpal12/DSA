6bdfd4191730526865eaa109bd00f6dbbbd517d0
Riya Nagpal <riyanagpal9c@gmail.com>
2026-05-06 22:33:05 +0530

    Create Longest_subarray_k.java

---
+++ Arrays/Striver/Longest_subarray_k.java
@@ -0,0 +1,20 @@
+public class subarray {
+  public static int longest_subarray(int arr[] , int k){
+    int start =0;
+    int ans =0;
+    int sum =0;
+    
+    for(int end =0 ;end<arr.length ;end++){
+      sum +=arr[end];
+
+      while (sum>k && start <= end){
+        sum -=arr[start];
+        start++;
+      }
+      if(sum == k){
+        ans = Math.max(ans , end-start+1);
+      }
+    }
+    return ans ; }
+  
+      
