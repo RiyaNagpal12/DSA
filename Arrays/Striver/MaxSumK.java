50503c3ae60d72341694bea62f14fd22c5f4c2d6
Riya Nagpal <riyanagpal9c@gmail.com>
2026-05-06 22:46:28 +0530

    Create MaxSumK.java

---
+++ Arrays/Striver/MaxSumK.java
@@ -0,0 +1,17 @@
+public class MaxSum_k{
+ public static int maxsum(int arr[] ,int k){
+     if (k > arr.length){
+       return -1; }
+    int currentsum =0;
+    
+    for(int i=0;i<k ;i++){
+      currentsum += arr[i];
+    }
+    int maxsum = currentsum ;
+    for(int i =k ;i<arr.length ;i++){
+      currentsum =  currentsum +arr[i] - arr[i-k];
+      maxsum = Math.max(currentsum , maxsum );
+    }
+    return maxsum ;
+  }
+  
