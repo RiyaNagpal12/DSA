ee077ce60d2e7b9d8c618e4d2e87cae329d3875f
Riya Nagpal <riyanagpal9c@gmail.com>
2026-05-08 19:16:50 +0530

    Update Leader.java

--- Arrays/Striver/Leader.java
+++ Arrays/Striver/Leader.java
@@ -2,11 +2,16 @@ public class Leader {
   public static void leader(int arr[] ){
     int max = arr[arr.length-1];
     System.out.print(max + " ");
-    for(int j= arr.length -2 ;j>=0 j--){
-      if (arr[i]> max){
-        max = arr[i] ;
+    for(int j= arr.length -2 ;j>=0 ; j--){
+      if (arr[j]> max){
+        max = arr[j] ;
         System.out.print( max +" " );
       }
     }
   }
+
+  public static void main(String[] args){
+    int arr[] ={16,17,4,3,5,2};
+    leader(arr);
+}
 }
