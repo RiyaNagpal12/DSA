24cecab5f62b95e02b739738a353bb7758172bc2
Riya Nagpal <riyanagpal9c@gmail.com>
2026-05-08 19:21:39 +0530

    Create Longest_Consecutive.java

---
+++ Arrays/Striver/Longest_Consecutive.java
@@ -0,0 +1,21 @@
+public  class Longest_Consecutive{
+  public static int count(int arr[]){
+    Arrays.sort(arr);
+    int count =0;
+    int maxcount =0;
+    for(int i=1  ; i<arr.length ;i++){
+      if (arr[i] == arr[i - 1]) {
+                continue;
+            }
+      if(arr[i] == arr[i-1] +1){
+        count++;
+        maxcount = Math.max(count , maxcount);
+      }
+      else{
+        count=0;
+      }
+
+    }
+    return maxcount ;
+  }
+  
