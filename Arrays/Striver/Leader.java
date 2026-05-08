bed8516bbc8bfe00c4354fd96c63048a45115ba8
Riya Nagpal <riyanagpal9c@gmail.com>
2026-05-08 19:15:30 +0530

    Create Leader.java

---
+++ Arrays/Striver/Leader.java
@@ -0,0 +1,12 @@
+public class Leader {
+  public static void leader(int arr[] ){
+    int max = arr[arr.length-1];
+    System.out.print(max + " ");
+    for(int j= arr.length -2 ;j>=0 j--){
+      if (arr[i]> max){
+        max = arr[i] ;
+        System.out.print( max +" " );
+      }
+    }
+  }
+}
