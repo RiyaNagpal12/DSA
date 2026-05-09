4da0578c4a0375727aba8d0ff5a27791e9673db2
Riya Nagpal <riyanagpal9c@gmail.com>
2026-05-09 23:21:28 +0530

    Create 4sum.java

---
+++ Arrays/Striver/4sum.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+
+public class Foursum {
+
+    public static void foursum(int arr[]) {
+        int n = arr.length;
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n - 3; i++) {
+
+            if (i > 0 && arr[i] == arr[i - 1]) continue;
+
+            for (int j = i + 1; j < n - 2; j++) {
+
+                if (j > i + 1 && arr[j] == arr[j - 1]) continue;
+
+                int left = j + 1;
+                int right = n - 1;
+
+                while (left < right) {
+
+                    int sum = arr[i] + arr[j] + arr[left] + arr[right];
+
+                    if (sum == 0) {
+                        System.out.println(arr[i] + " " + arr[j] + " " + arr[left] + " " + arr[right]);
+
+                        left++;
+                        right--;
+
+                        while (left < right && arr[left] == arr[left - 1]) left++;
+                        while (left < right && arr[right] == arr[right + 1]) right--;
+                    }
+
+                    else if (sum < 0) {
+                        left++;
+                    } 
+                    else {
+                        right--;
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 0, -1, 0, -2, 2};
+        foursum(arr);
+    }
+}
+      
