cf159029982b64c086d53005b3e61b7520578980
Riya Nagpal <riyanagpal9c@gmail.com>
2026-05-11 23:18:55 +0530

    Add binary search implementation in Find_Element.java
    Implement binary search to find an element in a sorted array.

---
+++ Arrays/Striver/Find_Element.java
@@ -0,0 +1,27 @@
+public class FindElement{
+  public static int findelement(int arr[]  , int start , int end , int target){
+    while(start<end){
+      int mid = (start+end)/2;
+      if(arr[mid]==target){
+        
+        return mid ;}
+      else if (arr[mid]<target){
+        findelement(arr , mid+1 , end , target);
+      }
+
+      else if(arr[mid]>target){
+        findelement(arr , start , mid-1 , target);
+
+      }
+
+        return -1
+
+          }
+
+  public static void main(String [] args){
+    int arr[]= { 2 , 5 ,6, 7, 9, 11, 15,19);
+    System.out.println(findelement(arr , 0 , arr.length -1 , 9));
+  }
+  }
+  
+    
