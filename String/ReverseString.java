08f23a76f185cd71532c4cbe523af237736b7fe2
Riya Nagpal <riyanagpal9c@gmail.com>
2026-05-15 23:09:08 +0530

    Create ReverseString.java

---
+++ String/ReverseString.java
@@ -0,0 +1,12 @@
+public class Reverse { 
+public static void reverse (String str){ 
+  String reverse ="";
+  for (int i= str.length()-1;i>=0;i--){
+    reverse = reverse + str.charAt(i);
+  } 
+  System.out.println(reverse);}
+  public static void main(String [] args ){ 
+  String str = "Riya";
+    reverse(str);
+  }
+}
