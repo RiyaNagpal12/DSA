1c4962f6e90596fe24507d67cb512d9cf92b9ba8
Riya Nagpal <riyanagpal9c@gmail.com>
2026-05-15 23:00:41 +0530

    Create LongestSubstring.java

---
+++ String/LongestSubstring.java
@@ -0,0 +1,29 @@
+import java.util.HashSet;
+
+public class LongestSubstring {
+
+    public static void longestSubstring(String str) {
+
+        HashSet<Character> set = new HashSet<>();
+
+        int left = 0;
+
+        for (int right = 0; right < str.length(); right++) {
+
+            while (set.contains(str.charAt(right))) {
+
+                set.remove(str.charAt(left));
+                left++;
+            }
+
+            set.add(str.charAt(right));
+        }
+
+        System.out.println(set);
+    }
+
+    public static void main(String[] args) {
+
+        longestSubstring("abcabcbb");
+    }
+}
