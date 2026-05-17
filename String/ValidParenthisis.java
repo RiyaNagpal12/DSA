d33b10769fe85de7968ae6fb5078fcd7042b6f2b
Riya Nagpal <riyanagpal9c@gmail.com>
2026-05-17 13:01:50 +0530

    Create ValidParenthisis.java

---
+++ String/ValidParenthisis.java
@@ -0,0 +1,46 @@
+import java.util.Stack;
+
+public class ValidParenthesis {
+
+    public static boolean isValid(String str) {
+
+        Stack<Character> st = new Stack<>();
+
+        for(int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+
+            // opening brackets
+            if(ch == '(' || ch == '[' || ch == '{') {
+
+                st.push(ch);
+            }
+
+            // closing brackets
+            else {
+
+                if(st.isEmpty()) {
+                    return false;
+                }
+
+                char top = st.pop();
+
+                if((ch == ')' && top != '(') ||
+                   (ch == ']' && top != '[') ||
+                   (ch == '}' && top != '{')) {
+
+                    return false;
+                }
+            }
+        }
+
+        return st.isEmpty();
+    }
+
+    public static void main(String[] args) {
+
+        String str = "{[()]}";
+
+        System.out.println(isValid(str));
+    }
+}
