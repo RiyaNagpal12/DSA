681de45380c5071bd195a4ac2acf61e505d401d0
Riya Nagpal <riyanagpal9c@gmail.com>
2026-05-18 23:10:59 +0530

    Add delete methods to SinglyLinkedList class
    Implement methods to delete a node from the beginning and by key in a singly linked list.

---
+++ LinkedLIst/SinglyLinkedList2.java
@@ -0,0 +1,46 @@
+public class Node { 
+int data ;
+Node next ; 
+Node (int data){
+  this.data = data ;
+  this.next = null; 
+}
+} 
+public class LinkedList{ 
+
+Node head ; 
+void deleteatbegining(){
+  if (head == null){
+    System.out.println(" Null");
+  return; 
+  } 
+  head = head.next ; 
+} 
+void delete(int key) {
+
+    // If list empty
+    if (head == null) {
+        System.out.println("List is empty");
+        return;
+    }
+
+    // If first node is key
+    if (head.data == key) {
+        head = head.next;
+        return;
+    }
+
+    Node temp = head;
+
+    while (temp.next != null && temp.next.data != key) {
+        temp = temp.next;
+    }
+
+    // Element not found
+    if (temp.next == null) {
+        System.out.println("Element not found");
+        return;
+    }
+
+    temp.next = temp.next.next;
+}
