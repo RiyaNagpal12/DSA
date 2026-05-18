ad70c95f88b2857892b65082bb6874bc211ae22f
Riya Nagpal <riyanagpal9c@gmail.com>
2026-05-18 23:59:11 +0530

    Update MaxPointCard.java

--- Arrays/Striver/MaxPointCard.java
+++ Arrays/Striver/MaxPointCard.java
@@ -1,1 +1,41 @@
+public class MaxPointCard {
 
+    public static int MaxPoint(int arr[], int k) {
+
+        int leftsum = 0;
+        int maxsum = 0;
+        int rightsum = 0;
+
+        int length = arr.length - 1;
+
+        // Take first k elements from left
+        for (int i = 0; i < k; i++) {
+            leftsum += arr[i];
+        }
+
+        maxsum = leftsum;
+
+        // Remove from left and add from right
+        for (int i = k - 1; i >= 0; i--) {
+
+            leftsum -= arr[i];
+            rightsum += arr[length];
+
+            maxsum = Math.max(maxsum, leftsum + rightsum);
+
+            length--;
+        }
+
+        return maxsum;
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = {6, 2, 3, 4, 7, 2, 1, 7, 1};
+        int k = 4;
+
+        int result = MaxPoint(arr, k);
+
+        System.out.println("Maximum Points = " + result);
+    }
+}
