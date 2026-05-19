888e6bde09180af24f9d5b45589612cc8cf11eb1
Riya Nagpal <riyanagpal9c@gmail.com>
2026-05-20 00:58:04 +0530

    Create AllSubstring.java

---
+++ String/AllSubstring.java
@@ -0,0 +1,9 @@
+public class Substring { 
+public static void genrate(String str){ 
+  for (int i =0;i<str.length ();i++){ 
+  string substring ="";
+    for (int j=i ; j< str.length();j++){ 
+    substring = substring + str.charAt(j);
+    }
+  } 
+  
