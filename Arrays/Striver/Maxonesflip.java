df50b8a54712c08ecd92d6a518ca2bca3008384b
Riya Nagpal <riyanagpal9c@gmail.com>
2026-05-20 23:02:14 +0530

    Update Maxonesflip.java

--- Arrays/Striver/Maxonesflip.java
+++ Arrays/Striver/Maxonesflip.java
@@ -1,27 +1,45 @@
-public class Maxones{ 
-public static in length(int arr[] , int k ){ 
-  int left =0; 
-  int right =0;
-  int zeroes =0;
-  int maxlength =0;
-  while (right < arr.length (){ 
-    if (arr[right]==0){
-      zeroes++; 
+public class Maxones {
+
+    public static int length(int arr[], int k) {
+
+        int left = 0;
+        int right = 0;
+        int zeroes = 0;
+        int maxlength = 0;
+
+        while (right < arr.length){
+
+            // count zeroes
+            if (arr[right] == 0) {
+                zeroes++;
+            }
+
+            // shrink window if zeroes > k
+            if(zeroes > k) {
+
+                if (arr[left] == 0) {
+                    zeroes--;
+                }
+
+                left++;
+            }
+
+            // calculate length
+            int length = right - left + 1;
+            maxlength = Math.max(length, maxlength);
+
+            right++;
+        }
+
+        return maxlength;
     }
-    if (zeroes > k ){
-      if (arr[right ]==0){
-        zeroes--;
-        left ++;
-      } 
-    } 
-    if (zeroes<=k){
-      int length = right-left ++;
-      maxlength = Math.max(length , maxlength );
-      
+
+    public static void main(String[] args) {
+
+        int arr[] = {1, 1, 0, 0, 1, 1, 1, 0, 1};
+        int k = 2;
+
+        System.out.println(length(arr, k));
     }
-    right ++; 
-  } 
-  return maxlength ;} 
+}
     
-       
-      
