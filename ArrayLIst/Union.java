05e306d09c957010c063f16a959b361800669212
Riya Nagpal <riyanagpal9c@gmail.com>
2026-05-20 23:33:53 +0530

    Update Union.java

--- ArrayLIst/Union.java
+++ ArrayLIst/Union.java
@@ -1,1 +1,52 @@
+public class union {
+  public static void union (int arr1[], arr2[]){
+    ArrayList<Integer> list = new ArrayList<>();
+    int i =0 ;
+    int j=0; 
+    while (i<=arr1.length && j<=arr2.length ){
+      if ( arr1[i] < arr2[j] ) {
+        if ( list.size() ==0 || list.get(list.size() -1)!=arr[i]){
+          list.add(arr[i]);}
+          i++;}
+      if ( arr1[i] > arr2[j] ) {
+        if ( list.size() ==0 || list.get(list.size() -1)!=arr[j]){
+      list.add(arr[j]);}
+          j++;} 
+    }
+    while (i<arr1.length){
+      if (list.get(list.size()-1)!= arr[i]{
+        list.add(arr[i] );
+      }
+      i++;}
+  }
+  j++;} 
+    }
+    while (j<arr2.length){
+      if (list.get(list.size()-1)!= arr[j]{
+        list.add(arr[j] );
+      }
+      j++;}
+  }
+for (int num : list) {
 
+            System.out.print(num + " ");
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        int arr1[] = {1, 2, 2, 3, 4, 5};
+
+        int arr2[] = {2, 3, 4, 4, 5, 6};
+
+        union(arr1, arr2);
+
+    }
+
+}
+
+
+
+        
