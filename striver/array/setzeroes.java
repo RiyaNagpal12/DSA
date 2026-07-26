1329816bf2b611adf2551bfe1e98e8cf3c28cc37
Riya Nagpal <riyanagpal9c@gmail.com>
2026-07-26 16:34:35 +0530

    Add setZeroes method to handle zeroes in matrix
    Implement setZeroes method to set entire row and column to zero if an element is zero.

---
+++ striver/array/setzeroes.java
@@ -0,0 +1,44 @@
+public class solution{
+  public static void setzeroes(int nums[][] ){
+    int n = nums.length;
+    int m = nums[0].length ;
+    int col0 = 1;
+
+  for(int i = 0;i<n ;i++){
+    for(int j =0 ;j<m ;j++){
+      if(nums[i][j] == 0){
+        nums[i][0] = 0;
+
+        if(j!=0){
+          nums[0][j] =0;
+      }
+        else {
+          count0=0;
+        }
+      }
+    }
+
+    for(int i =1 ;i<n ;i++){
+      for(int j =1 ;j<m ;j++){
+        if(nums[i][j] !=0){
+          if(nums[i][0]==0 || nums[0][j] ==0){
+            nums[i][j]=0;
+          }
+        }
+      }
+    }
+   if(nums[0][0] == 0){
+            for(int j = 0; j < m; j++){
+                nums[0][j] = 0;
+            }
+        }
+
+        // First column
+        if(col0 == 0){
+            for(int i = 0; i < n; i++){
+                nums[i][0] = 0;
+            }
+        }
+    }
+}
+      
