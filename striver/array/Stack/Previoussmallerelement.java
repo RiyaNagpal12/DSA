f13e8e056792a2dffd6117426a2672b336bb0b97
Riya Nagpal <riyanagpal9c@gmail.com>
2026-08-18 21:42:40 +0530

    Create Previoussmallerelement.java

---
+++ striver/array/Stack/Previoussmallerelement.java
@@ -0,0 +1,20 @@
+public class solution{
+  public static int[] previoussmaller(int nums[]){
+    int pse[]=new int[nums.length];
+    Stack<Integer> st = new Stack();
+    for(int i= 0;i<nums.length;i++){
+      while(!st.isEmpty() && st.peek()>=nums[i]){
+        st.pop();
+      }
+      if(st.isEmpty()){
+        pse[i]=-1;
+      }
+      else {
+        pse[i]=st.peek();
+      }
+      st.push(nums[i]);
+    }
+
+    return pse;
+  }
+}
